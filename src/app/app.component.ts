9a4244f786f9736d70be93fa54e35fc0cce3d2ad
chandu77k <chandukasani77@icloud.com>
2025-03-14 10:51:24 +0530

    fetch data in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MartComponent } from './mart/mart.component';
+import { FetchComponent } from './fetch/fetch.component';
+import { provideHttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MartComponent],
+  imports: [RouterOutlet,MartComponent,FetchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/fetch/fetch.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { GetDataService } from './get-data.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch',
+  standalone: true,
+  imports: [CommonModule],
+  providers: [GetDataService],
+  templateUrl: './fetch.component.html',
+  styleUrls: ['./fetch.component.css']
+})
+export class FetchComponent implements OnInit {
+  data: any;
+
+  constructor(private dataService: GetDataService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.dataService.fetchData()
+      .subscribe(response => {
+        this.data = response;
+      });
+  }
+}
